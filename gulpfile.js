ab2d979f1e016815afd2327e51bb88a166953635
Joshua Apstein <josh4locations@gmail.com>
2017-01-20 14:34:23 -0800

    Made gulpfile work for async issues. One issue with bundle, can't figure out line 18658

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+var gulp = require('gulp');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var notify = require('gulp-notify');
+
+function handleErrors() {
+  notify.onError({
+    title: 'Compile Error',
+    message: '<%= error.message %>'
+  }).apply(this, arguments); // eslint-disable-line
+  this.emit('end'); // keeps gulp from hanging on this task
+}
+
+function buildScript(file) {
+  var props = {
+    entries: ['./' + file],
+    debug: true,
+        transform: [
+      ['babelify', { presets: ['es2015'] }]
+    ]
+  };
+
+  var bundler = browserify(props);
+
+  function bundle() {
+    var stream = bundler.bundle();
+    return stream
+      .on('error', handleErrors)
+      .pipe(source('bundle.js'))
+      .pipe(gulp.dest('./build'));
+  }
+  // run it once the first time buildScript is called
+  return bundle();
+}
+
+gulp.task('default', () => buildScript('./lib/agent.js', false)); 
+
