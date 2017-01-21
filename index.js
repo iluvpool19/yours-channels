b72404cd006a36d28551303ab9c153b296e5e811
Joshua Apstein <josh4locations@gmail.com>
2017-01-21 12:03:03 -0800

    added bundle to gitignore

--- index.js
+++ index.js
@@ -6,11 +6,11 @@
  * let Agent = require('yours-channels').Agent
  */
 const lib = {};
+
 lib.Agent = require('./lib/agent.js');
 lib.Channel = require('./lib/channel.js');
 lib.Consts = require('./lib/consts.js');
 lib.Output = require('./lib/output.js');
 lib.Wallet = require('./lib/wallet.js');
-
 
 module.exports = lib;
