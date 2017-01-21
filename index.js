08d03f5d48daa2289751daa8b7b7b51fa09049e0
Joshua Apstein <josh4locations@gmail.com>
2017-01-21 11:54:02 -0800

    Make Agent/Channel classes available

--- index.js
+++ index.js
@@ -5,4 +5,12 @@
  *
  * let Agent = require('yours-channels').Agent
  */
-module.exports.agent = require('lib/agent')
+const lib = {};
+lib.Agent = require('./lib/agent.js');
+lib.Channel = require('./lib/channel.js');
+lib.Consts = require('./lib/consts.js');
+lib.Output = require('./lib/output.js');
+lib.Wallet = require('./lib/wallet.js');
+
+
+module.exports = lib;
